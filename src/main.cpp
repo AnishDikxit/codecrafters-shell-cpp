6aa101028590c1390fba0cd2dfe128fb5e585079
Anish Dixit <anish.22053@gmail.com>
2025-09-18 23:25:26 +0530

    Trying with another approach

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <string>
-
+using namespace std;
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
   // Uncomment this block to pass the first stage
   while(true){
-    std::cout << "$ ";
-    std::string input;
-    std::cin.ignore(5);
-    std::getline(std::cin, input);
-    std::cout<< input <<std::endl;
+    cout << "$ ";
+    string input;
+    getline(std::cin, input);
+    if(input == "exit 0")return 0;
+    if(input.find("echo")!=string::npos){
+      cout<<input.substr(5)<<endl;
+      continue;
+    }
+    cout<<input<<": command not found"<<endl;
   }
 }
