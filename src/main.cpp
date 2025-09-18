5e76acf11c1ceff044a2789fd60b65086447ad28
Anish Dixit <anish.22053@gmail.com>
2025-09-18 22:44:48 +0530

    Testing exit 0

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,11 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-  while(true){
-    std::cout << "$ ";
-    std::string input;
-    std::getline(std::cin, input);
-    std::cout<<input << ": command not found" << std::endl;
-  }
   // Uncomment this block to pass the first stage
-  // std::cout << "$ ";
-  // std::string input;
-  // std::getline(std::cin, input);
-  // std::cout<<input << ": command not found" << std::endl;
+  std::cout << "$ ";
+  std::string input, test;
+  std::getline(std::cin, input);
+  std::cout<<input << ": command not found" << std::endl;
+  std::getline(std::cin, test);
+  return 0;
 }
