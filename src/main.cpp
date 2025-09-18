8221c5c8e03bd98ba38b6491792e2b0cf044a483
Anish Dixit <anish.22053@gmail.com>
2025-09-18 22:16:25 +0530

    Trying REPL in Shell

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,15 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
+  while(true){
+    std::cout << "$ ";
+    std::string input;
+    std::getline(std::cin, input);
+    std::cout<<input << ": command not found" << std::endl;
+  }
   // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout<<input << ": command not found" << std::endl;
+  // std::cout << "$ ";
+  // std::string input;
+  // std::getline(std::cin, input);
+  // std::cout<<input << ": command not found" << std::endl;
 }
