b000b9eaa850d7a3353edacce47691af0158aa51
Anish Dixit <anish.22053@gmail.com>
2025-09-19 23:09:08 +0530

    Trying this stage

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
 using namespace std;
 int main() {
   // Flush after every std::cout / std:cerr
@@ -10,9 +12,27 @@ int main() {
     cout << "$ ";
     string input;
     getline(std::cin, input);
+    std::stringstream ss(input);
     if(input == "exit 0")return 0;
-    if(input.find("echo")!=string::npos){
-      cout<<input.substr(5)<<endl;
+    string token;
+    vector<string> tokens;
+    while(ss>>token){
+      tokens.push_back(token);
+    }
+    if(tokens[0]=="type"){
+      if(tokens[1]=="type" || tokens[1]=="echo" || tokens[1]=="exit"){
+        cout<<tokens[1]<<" "<<"is a shell builtin"<<endl;
+      }
+      else{
+        cout<<tokens[1]<<": "<<"not found"<<endl;
+      }
+      continue;
+    }
+    if(tokens[0]=="echo"){
+      for(int i = 1; i<(int)tokens.size()-1; i++){
+        cout<<tokens[i]<<" ";
+      }
+      cout<<tokens[(int)tokens.size()-1]<<endl;
       continue;
     }
     cout<<input<<": command not found"<<endl;
